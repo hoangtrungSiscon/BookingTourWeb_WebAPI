b86212caafa88617fcd483ae6ac28c85e10624e5
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2023-10-30 18:47:17 +0700

    Add project files.

---
+++ BookingTourWeb_WebAPI/Program.cs
@@ -0,0 +1,29 @@
+global using BookingTourWeb_WebAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DvmayBayContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
