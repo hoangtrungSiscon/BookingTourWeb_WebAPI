b7dfb9e028b404f46890bbb17283736682e66a18
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-08-05 12:09:26 +0700

    add vnpay services

---
+++ BookingTourWeb_WebAPI/Services/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using BookingTourWeb_WebAPI.Models;
+
+namespace BookingTourWeb_WebAPI.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, int id, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
